c187b1c44b572d3958514438e83912fb559c3a63
Usman <55202183+usmankamayl@users.noreply.github.com>
2024-07-29 10:04:03 +0300

    Update API.js

--- src/scripts/API.js
+++ src/scripts/API.js
@@ -19,7 +19,7 @@ const formatSearchParams = params => {
 
     return `?${searchParams.toString()}`;
 }
-export const API_URL = 'https://mirano-api-tuhm.onrender.com';
+export const API_URL = 'https://mirano-api-tuhm.onrender.com/';
 export const fetchProducts = async (params = {}) => {
   try {
       const response = await fetch(`${API_URL}/api/products${formatSearchParams(params)}`);
